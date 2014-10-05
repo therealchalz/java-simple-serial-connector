fec9fda0dae4950690cfaa3f282bb473091c8df3
Charles Hache <chache@brood.ca>
2014-10-04 23:58:43 -0700

    Disable port opening when listing ports

--- src/java/jssc/SerialPortList.java
+++ src/java/jssc/SerialPortList.java
@@ -330,13 +330,14 @@ public class SerialPortList {
                     String fileName = file.getName();
                     if(!file.isDirectory() && !file.isFile() && pattern.matcher(fileName).find()){
                         String portName = searchPath + fileName;
+                        /*
                         long portHandle = serialInterface.openPort(portName, false);//Open port without TIOCEXCL
                         if(portHandle < 0 && portHandle != SerialNativeInterface.ERR_PORT_BUSY){
                             continue;
                         }
                         else if(portHandle != SerialNativeInterface.ERR_PORT_BUSY) {
                             serialInterface.closePort(portHandle);
-                        }
+                        }*/
                         portsTree.add(portName);
                     }
                 }
